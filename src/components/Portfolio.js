98b93bd947c23f546b4067ac8100a219e34d086c
jdhova <juud0010@gmail.com>
2022-08-01 09:37:03 +0500

    deploy

---
+++ src/components/Portfolio.js
@@ -0,0 +1,212 @@
+// Portfolio.js
+
+import React from 'react'
+
+export const Portfolio = () => {
+  return (
+	<section id="section-portfolio" className="page-section topmargin-lg bg--secondary">
+
+					<div className="heading-block center">
+						<h2>Our Works</h2>
+						<span>Some of the Awesome Projects we've worked on.</span>
+					</div>
+
+          <div className="container clearfix">
+
+						
+						<ul id="portfolio-filter" className="portfolio-filter clearfix" data-container="#portfolio">
+
+							<li className="activeFilter"><a href="/#" data-filter="*">Show All</a></li>
+							<li><a href="/#" data-filter=".node">Node Js</a></li>
+							<li><a href="/#" data-filter=".react">REACT-JS</a></li>
+							<li><a href="/#" data-filter=".js">JAVASCRIPT</a></li>
+
+						</ul>
+
+						
+
+						<div className="clear"></div>
+
+					
+						<div id="portfolio" className="portfolio grid-container portfolio-nomargin clearfix">
+
+							<article className="portfolio-item react pf-icons">
+								<div className="portfolio-image">
+									<a href="/#">
+										<img src="images/portfolio/thumb-1.jpg" alt="Open Imagination" />
+									</a>
+									<div className="portfolio-overlay">
+										<a href="images/portfolio/thumb-1.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-eye"></i></a>
+										<a href="/#" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div className="portfolio-desc">
+									<h3><a href="/#">ChowNow Ordering</a></h3>
+									<span><a href="/#">
+										React JS</a></span>
+								</div>
+							</article>
+
+							
+
+							<article className="portfolio-item react node pf-icons">
+								<div className="portfolio-image">
+									<a href="/#">
+										<img src="images/portfolio/thumb-2.jpg" alt="Open Imagination" />
+									</a>
+									<div className="portfolio-overlay">
+										<a href="images/portfolio/thumb-2.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-eye"></i></a>
+										<a href="/#" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div className="portfolio-desc">
+									<h3><a href="/#">ChowNow Discover
+										</a></h3>
+									<span><a href="/#">React JS </a>, <a href="/#">Node</a></span>
+								</div>
+							</article>
+
+								
+
+							<article className="portfolio-item node react pf-icons">
+								<div className="portfolio-image">
+									<a href="/#">
+										<img src="images/portfolio/thumb-3.jpg" alt="Open Imagination" />
+									</a>
+									<div className="portfolio-overlay">
+										<a href="images/portfolio/thumb-3.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-eye"></i></a>
+										<a href="/#" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div className="portfolio-desc">
+									<h3><a href="/#">New Relic
+										
+										</a></h3>
+									<span><a href="/#">React</a> , <a href="/#"> Node Js</a></span>
+								</div>
+							</article>
+
+	
+
+							<article className="portfolio-item js pf-icons">
+								<div className="portfolio-image">
+									<a href="/#">
+										<img src="images/portfolio/thumb-4.jpg" alt="Open Imagination" />
+									</a>
+									<div className="portfolio-overlay">
+										<a href="images/portfolio/thumb-4.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-eye"></i></a>
+										<a href="/#" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div className="portfolio-desc">
+									<h3><a href="/#">Roambi
+										
+										
+										</a></h3>
+									<span><a href="/#">JavaScript</a></span>
+								</div>
+							</article>
+	
+
+							<article className="portfolio-item node js pf-icons">
+								<div className="portfolio-image">
+									<a href="/#">
+										<img src="images/portfolio/thumb-5.jpg" alt="Open Imagination" />
+									</a>
+									<div className="portfolio-overlay">
+										<a href="images/portfolio/thumb-5.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-eye"></i></a>
+										<a href="/#" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div className="portfolio-desc">
+									<h3><a href="/#">Walker Tracker
+										
+										
+										
+										</a></h3>
+									<span><a href="/#">JS </a> , <a href="/#" >Node Js</a></span>
+								</div>
+							</article>
+							
+							<article className="portfolio-item js node pf-icons">
+								<div className="portfolio-image">
+									<a href="/#">
+										<img src="images/portfolio/thumb-6.jpg" alt="Open Imagination" />
+									</a>
+									<div className="portfolio-overlay">
+										<a href="images/portfolio/thumb-6.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-eye"></i></a>
+										<a href="/#" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div className="portfolio-desc">
+									<h3><a href="/#">MY STAND
+										
+										
+										
+										</a></h3>
+									<span><a href="/#">JS </a> , <a href="/#" >Node.js on Sails</a></span>
+								</div>
+							</article>
+							
+							<article className="portfolio-item js pf-icons">
+								<div className="portfolio-image">
+									<a href="/#">
+										<img src="images/portfolio/thumb-7.jpg" alt="Open Imagination" />
+									</a>
+									<div className="portfolio-overlay">
+										<a href="images/portfolio/thumb-7.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-eye"></i></a>
+										<a href="/#" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div className="portfolio-desc">
+									<h3><a href="/#">NEVER SURRENDER
+										
+										</a></h3>
+									<span><a href="/#">JavaScript </a></span>
+								</div>
+							</article>
+							
+							<article className="portfolio-item react node pf-icons">
+								<div className="portfolio-image">
+									<a href="/#">
+										<img src="images/portfolio/thumb-8.jpg" alt="Open Imagination" />
+									</a>
+									<div className="portfolio-overlay">
+										<a href="images/portfolio/thumb-8.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-eye"></i></a>
+										<a href="/#" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div className="portfolio-desc">
+									<h3><a href="/#">Powur
+										
+										
+										</a></h3>
+									<span><a href="/#">React Js  </a> , <a href="/#" >Node Js</a></span>
+								</div>
+							</article>
+							
+							<article className="portfolio-item react pf-icons">
+								<div className="portfolio-image">
+									<a href="/#">
+										<img src="images/portfolio/thumb-9.jpg" alt="Open Imagination" />
+									</a>
+									<div className="portfolio-overlay">
+										<a href="images/portfolio/thumb-9.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-eye"></i></a>
+										<a href="/#" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+									</div>
+								</div>
+								<div className="portfolio-desc">
+									<h3><a href="/#">The Mall</a></h3>
+									<span><a href="/#">React.js  </a> , <a href="/#" >Node Js</a></span>
+								</div>
+							</article>
+						</div>
+
+
+					</div>
+
+				</section>
+  )
+}
+
+export default Portfolio
